d0c39259c002fa44768626ff04ecc43c91e49d03
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2016-10-30 03:59:55 +0200

    Final adjustments. Went one hour overtime, oh well.

--- morsetap/src/GameView.hx
+++ morsetap/src/GameView.hx
@@ -1,8 +1,11 @@
 package;
+import haxe.Timer;
 import js.Lib;
 import pixi.core.display.Container;
 import pixi.core.graphics.Graphics;
 import pixi.core.sprites.Sprite;
+import pixi.core.text.Text;
+import pixi.core.text.TextStyleObject;
 
 /**
  * ...
@@ -10,6 +13,8 @@ import pixi.core.sprites.Sprite;
  */
 class GameView extends Container
 {
+	public var gameover:Text;
+	
 	private var character:Character;
 	private var bg:Graphics;
 	
@@ -31,6 +36,15 @@ class GameView extends Container
 	public function new() 
 	{
 		super();
+		var tf:TextStyleObject = { };
+		tf.fontFamily = "Impact";
+		tf.fill = 0xFFFFFF;
+		tf.fontSize = 150;
+		tf.strokeThickness = 15;
+		tf.stroke = 0x0;
+		
+		this.gameover = new Text("GAME OVER " , tf);
+		
 		this.ground = new Graphics();
 		this.character = new Character();
 		this.bg = new Graphics();
@@ -51,14 +65,17 @@ class GameView extends Container
 		this.character.y = 620 + 10;
 		this.character.x = 280;
 		
+		
 		this.addListener("mousedown", ondown);
 		this.addListener("touchstart", ondown);
 		this.addListener("mouseup", onup);
 		this.addListener("touchend", onup);
 	}
-	public function prepare():Void
+	private var offset:Int = 0;
+	public function prepare(?replay:Bool):Void
 	{
 		wc = 0;
+		startimeset = false;
 		speed = 1;
 		for (r in columns)
 			this.removeChild(r);
@@ -74,12 +91,19 @@ class GameView extends Container
 			wc +=cast mc.width;
 			columns.push(mc);
 		}
+		if (replay)
+		{
+			for (c in columns)
+				c.x -= 2000;
+			wc -= 2000;
+		}
 		var startline:Graphics = new Graphics();
 		startline.beginFill(Main.HILIGHT);
 		startline.drawRect(0, 0, 5, 620);
 		startline.endFill();
-		startline.x = mc.width;
+		startline.x = mc.width-5;
 		mc.addChild(startline);
+		this.gameover.visible = false;
 		this.addChild(this.ground);
 		this.addChild(this.character);
 		
@@ -103,6 +127,9 @@ class GameView extends Container
 	
 	private function ondown():Void
 	{
+		if (!this.interactive) return;
+		if (this.gameover.visible) prepare(true);
+		this.gameover.visible = false;
 		if (!started) this.start = Date.now().getTime();
 		lastd =cast Date.now().getTime();
 		this.started = true;
@@ -113,16 +140,20 @@ class GameView extends Container
 	{
 		if (Date.now().getTime() - lastd > 5000)
 		{
-			Lib.alert("Dont cheat!");
 			started = false;
-			prepare();
+			prepare(true);
+			gameover.text = "DONT CHEAT!";
+			gameover.visible = true;
+			this.addChild(gameover);
+			gameover.x = Math.round((1280 - gameover.width) /2);
+			gameover.y = Math.round((720 - gameover.height) /2);
 		}
 		down = false;
 		this.character.down();
 	}
 	
-	var wordlist:String = " KEEP ON JAMMING --- CAN YOU BEAT THIS GAME --- THIS MESSAGE LOOPS --- ";
-	
+	var wordlist:String = " KEEP ON JAMMING - CAN YOU BEAT THIS GAME - THIS MESSAGE LOOPS - ";
+	private var startimeset:Bool = false;
 	public function ontick(delta:Float):Void
 	{
 		if (!started) return;
@@ -132,13 +163,23 @@ class GameView extends Container
 				c.visible = false;
 				continue;
 			}
+			if (c.x < character.x && !c.tutorial && !startimeset)
+			{
+				startimeset = true;
+				this.start = Date.now().getTime();
+			}
 			c.x -= delta * 2*speed;
 			if (!c.tutorial && c.x + c.width < character.x && c.blocks.length > 0)
 			{
 				var time:Int = Math.round((Date.now().getTime() - start)/1000);
-				Lib.alert("Game over. Your time was " + time);
+				gameover.text = "GAME OVER\nSCORE: " + time;
+		this.interactive = false;
+		Timer.delay(function() { this.interactive = true; }, 400);
 				started = false;
-				prepare();
+				gameover.visible = true;
+				this.addChild(gameover);
+				gameover.x = Math.round((1280 - gameover.width) /2);
+				gameover.y = Math.round((720 - gameover.height) /2);
 			}
 		}
 		wc-= delta * 2*speed;
